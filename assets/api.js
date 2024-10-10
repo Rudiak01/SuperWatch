3cc79f98fe1645410f6134c8754b92936dfc5aa3
Rudiak <romain.mottard@gmail.com>
2024-10-10 17:16:36 +0200

    api create

--- assets/api.js
+++ assets/api.js
@@ -2,8 +2,34 @@ fetch('http://arthonetwork.fr:8001/api')
 .then(response => response.json())
 .then(data => tri(data));
 
+
 function tri(data){
-    console.log(data);
+    var nb = 0;
+    for (players in data.players.list) {
+        addElement();
+    }
+    console.log(nb);}
+function addElement() {
+    var article = document.createElement("article");
+    var img = document.createElement("img");
+    var pseudo = document.createElement("pseudo");
+    var uuid = document.createElement("uuid")
+    var version = document.createElement("version");
+    article.className = "etiquette";
+    img.src="assets/body.png";img.height="170";img.width="170";
+    pseudo.innerHTML ="pseudo"; pseudo.className="username";
+    uuid.innerHTML="uuid";
+    version.innerHTML="version";
+    const parent = document.querySelector("section");
+    parent.appendChild(article);
+    article.appendChild(img);
+    article.appendChild(pseudo);
+    article.appendChild(uuid);
+    article.appendChild(version);
+}
+
+/*
+
     const playerCounter = document.getElementById("player-counter");
     playerCounter.innerHTML = data.players.online;
 
@@ -12,4 +38,4 @@ function tri(data){
     player.innerHTML = data.players.list.Rudiak.uuid;
     const playerusername = document.getElementById("username");
     playerusername.innerHTML = data.players.list;
-} 
+*/
---
+++ assets/navbar.js
@@ -0,0 +1,27 @@
+const menuBtn = document.getElementById('menu-btn'); // Sélection du bouton du menu
+const overlayMenu = document.getElementById('overlay-menu'); // Sélection de l'overlay menu
+const overlayBg = document.getElementById('overlay-bg'); // Sélection de l'overlay background
+let isMenuOpen = false; // Variable de suivi pour l'état du menu
+
+menuBtn.addEventListener('click', () => {
+    if (!isMenuOpen) {
+      overlayBg.classList.add('open');  // Fade in background
+      overlayMenu.classList.add('open');  // Slide in menu
+      menuBtn.innerHTML = '&times;'; // Change icon to "close"
+    } else {
+      overlayBg.classList.remove('open');  // Fade out background
+      overlayMenu.classList.remove('open');  // Slide out menu
+      menuBtn.innerHTML = '&#9776;'; // Change back to "menu" icon
+    }
+    isMenuOpen = !isMenuOpen; // Toggle the state
+  });
+
+// Optional: Close the menu when clicking on the background
+overlayBg.addEventListener('click', () => {
+  if (isMenuOpen) {
+    overlayBg.classList.remove('open');
+    overlayMenu.classList.remove('open');
+    menuBtn.innerHTML = '&#9776;';
+    isMenuOpen = false;
+  }
+});
