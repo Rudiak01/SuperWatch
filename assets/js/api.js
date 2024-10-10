c85580bd0b60ad95b6343b6d2f05c6a1bcf6af53
Rudiak <romain.mottard@gmail.com>
2024-10-10 22:10:37 +0200

    player box

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,41 +1,46 @@
 fetch('http://arthonetwork.fr:8001/api')
-.then(response => response.json())
-.then(data => tri(data));
+    .then(response => response.json())
+    .then(data => addElement(data));
 
 
-function tri(data){
-    var nb = 0;
-    for (players in data.players.list) {
-        addElement();
+
+function addElement(data) {
+    for (const playerName in data.players.list) {
+        if (data.players.list.hasOwnProperty(playerName)) {
+            const player = data.players.list[playerName];
+            console.log(`Nom du joueur: ${playerName}`);
+            console.log(`Statut: ${player.status}`);
+            console.log(`uuid : ${player.uuid}`);
+            console.log("-----");
+            var article = document.createElement("article");
+            var skin = document.createElement("img");
+            var text = document.createElement("div");
+            var pseudo = document.createElement("p");
+            var uuid = document.createElement("p")
+            var version = document.createElement("p");
+
+            article.className = "etiquette";
+            skin.src = `https://mineskin.eu/armor/body/${playerName}/100.png`;
+            text.className = "text-etiquette";
+            pseudo.innerHTML = `${playerName}`; pseudo.className = "username";
+            uuid.innerHTML = `UUID : ${player.uuid}`; uuid.className = "uuid";
+            version.innerHTML = "version **API**"; version.className = "version";
+            const parent = document.querySelector("section");
+            parent.appendChild(article);
+            article.appendChild(skin);
+            article.appendChild(text)
+            text.appendChild(pseudo);
+            text.appendChild(uuid);
+            text.appendChild(version);
+            if (player.status === "online") {
+                article.style.boxShadow = " lime 0px 0px 0px 3px";
+            }
+            else {
+                article.style.boxShadow = " red 0px 0px 0px 3px";
+            }
+        }
     }
-    console.log(nb);}
-function addElement() {
-    var article = document.createElement("article");
-    var img = document.createElement("img");
-    var pseudo = document.createElement("pseudo");
-    var uuid = document.createElement("uuid")
-    var version = document.createElement("version");
-    article.className = "etiquette";
-    img.src="assets/body.png";img.height="170";img.width="170";
-    pseudo.innerHTML ="pseudo"; pseudo.className="username";
-    uuid.innerHTML="uuid";
-    version.innerHTML="version";
-    const parent = document.querySelector("section");
-    parent.appendChild(article);
-    article.appendChild(img);
-    article.appendChild(pseudo);
-    article.appendChild(uuid);
-    article.appendChild(version);
+
 }
 
-/*
 
-    const playerCounter = document.getElementById("player-counter");
-    playerCounter.innerHTML = data.players.online;
-
-
-    const player = document.getElementById("UUID");
-    player.innerHTML = data.players.list.Rudiak.uuid;
-    const playerusername = document.getElementById("username");
-    playerusername.innerHTML = data.players.list;
-*/
