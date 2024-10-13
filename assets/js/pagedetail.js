03f8e22345f2964d00ce3abd3d6e4d1facec84a5
Rudiak <romain.mottard@gmail.com>
2024-10-13 22:09:35 +0200

    name transfer

--- assets/js/pagedetail.js
+++ assets/js/pagedetail.js
@@ -1,20 +1,22 @@
-window.onload = function () {
-    var url = document.location.href,
-        params = url.split('?')[1].split('&'),
-        data = {}, tmp;
-    for (var i = 0, l = params.length; i < l; i++) {
-        tmp = params[i].split('=');
-        data[tmp[0]] = tmp[1];
-    }
-    document.getElementById('here').innerHTML = data.name;
-}
 fetch('http://arthonetwork.fr:8001/api')
     .then(response => response.json())
     .then(data => Init(data));
 
-function Init(data) {
+function Init(data,) {
     // ----- Copy Pasta All js files ----- //
     const playerCounter = document.getElementById("navbar-counter");
     playerCounter.innerHTML = data.players.online;
     // ----------------------------------- //
+    var url = document.location.href,
+        params = url.split('?')[1].split('&'),
+        transfer = {}, tmp;
+    for (var i = 0, l = params.length; i < l; i++) {
+        tmp = params[i].split('=');
+        transfer[tmp[0]] = tmp[1];
+    }
+    document.getElementById('here').innerHTML = transfer.name;
+    var skin = document.createElement("img");
+    const parent = document.querySelector("main");
+    skin.src = `https://mineskin.eu/armor/body/${transfer.name}/100.png`;
+    parent.appendChild(skin);
 }
