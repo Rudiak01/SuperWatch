94b0536aa3db6299fa924e1b372741bceea2c0d8
Rudiak <romain.mottard@gmail.com>
2025-02-06 20:15:37 +0100

    first try at a tutorial

--- assets/js/landing_page.js
+++ assets/js/landing_page.js
@@ -3,22 +3,25 @@ const totalSteps = 3;
 
 function showStep(step) {
   for (let i = 1; i <= totalSteps; i++) {
-    document.getElementById(`Step${i}`).style.display = i === step ? 'block' : 'none';
+    document.getElementById(`Step${i}`).style.display =
+      i === step ? "block" : "none";
   }
-  
-  document.getElementById('prevBtn').style.display = step > 1 ? 'inline-block' : 'none';
-  document.getElementById('nextBtn').style.display = step <= totalSteps ? 'inline-block' : 'none';
-  
+
+  document.getElementById("prevBtn").style.display =
+    step > 1 ? "inline-block" : "none";
+  document.getElementById("nextBtn").style.display =
+    step <= totalSteps ? "inline-block" : "none";
+
   if (step === totalSteps) {
-    document.getElementById('nextBtn').textContent = 'Terminer';
-    document.getElementById('nextBtn').href = 'index.html';
+    document.getElementById("nextBtn").textContent = "Terminer ↓";
+    document.getElementById("nextBtn").href = "index.html";
   } else {
-    document.getElementById('nextBtn').textContent = 'Suivant →';
-    document.getElementById('nextBtn').href = '#';
+    document.getElementById("nextBtn").textContent = "Suivant →";
+    document.getElementById("nextBtn").href = "#";
   }
 }
 
-document.getElementById('nextBtn').addEventListener('click', function(e) {
+document.getElementById("nextBtn").addEventListener("click", function (e) {
   if (currentStep < totalSteps) {
     e.preventDefault();
     currentStep++;
@@ -26,7 +29,7 @@ document.getElementById('nextBtn').addEventListener('click', function(e) {
   }
 });
 
-document.getElementById('prevBtn').addEventListener('click', function(e) {
+document.getElementById("prevBtn").addEventListener("click", function (e) {
   e.preventDefault();
   if (currentStep > 1) {
     currentStep--;
